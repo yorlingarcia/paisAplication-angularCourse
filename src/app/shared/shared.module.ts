0f1e82a9929e62c3cbfd7b4be81c3077273f848a
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-20 17:03:26 -0500

    se cambio la forma de importar en el modulo, debido a que el modulo solo se importa una vezx la redundacioa se acepta y angular solo lo usa una sola vez

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SidebarComponent],
   exports: [SidebarComponent],
-  imports: [CommonModule, AppRoutingModule],
+  imports: [CommonModule, RouterModule],
 })
 export class SharedModule {}
