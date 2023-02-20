a2c0e6d6775b431a4679c62c2fae47d125cf0725
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-20 18:33:51 -0500

    se crearon los servicios para usar la api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PaisModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PaisModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,8 +8,11 @@ import { Component } from '@angular/core';
 })
 export class PorPaisComponent {
   termino: string = '';
-  constructor() {}
+  constructor(private paisService: PaisService) {}
   buscar() {
     console.log(this.termino);
+    this.paisService.buscarPais(this.termino).subscribe((resp) => {
+      console.log(resp);
+    });
   }
 }
