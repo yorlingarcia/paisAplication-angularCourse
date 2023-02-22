d164780791dc9a3fab63a93148e37c2df092697a
Yorlin Garcia <yorlingarcia96@hotmail.com>
2023-02-21 22:39:26 -0500

    se agrego un filtrado a la peticion para no sobrecargar de peticiones inecesarias

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaisService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  get httpParams() {
+    return new HttpParams().set(
+      'fields',
+      'capital,name,population,capital,flags'
+    );
+  }
+
   constructor(private http: HttpClient) {}
 
   buscarPais(termino: string): Observable<Country[]> {
@@ -16,8 +25,12 @@ export class PaisService {
   }
 
   buscarRegion(termino: string): Observable<Country[]> {
+    const params = new HttpParams().set(
+      'fields',
+      'capital,name,population,capital,flags'
+    );
     const url: string = `${this.apiUrl}/region/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, { params }).pipe(tap(console.log));
   }
 
   buscarCapital(termino: string): Observable<Country[]> {
@@ -29,4 +42,7 @@ export class PaisService {
     const url: string = `${this.apiUrl}/alpha/${id}`;
     return this.http.get<Country[]>(url);
   }
+}
+function tag(): import('rxjs').OperatorFunction<Country[], Country[]> {
+  throw new Error('Function not implemented.');
 }
